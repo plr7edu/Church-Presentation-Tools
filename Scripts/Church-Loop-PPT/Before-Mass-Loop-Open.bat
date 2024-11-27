fb3887e4956385449d089543f832bf5bf691aadd
plr7edu <plr7.edu@outlook.com>
2024-11-27 15:32:07 +0530

    update Before-Mass-Loop things

--- Scripts/Church-Loop-PPT/Before-Mass-Loop-Open.bat
+++ Scripts/Church-Loop-PPT/Before-Mass-Loop-Open.bat
@@ -31,6 +31,6 @@
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
-start powerpnt.exe "%SYSTEMDRIVE%\Church-Presentation-Tools\Scripts\Church-Loop-PPT\Before-Mass-Loop.pptx"
+start powerpnt.exe "%HOMEPATH%\Documents\Before-Mass-Loop\Before-Mass-Loop.pptx"
 
 
