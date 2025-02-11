4fbcbedeb457fbd5812c72531238c91b57800226
plr7edu <plr7.edu@outlook.com>
2025-02-12 00:23:09 +0530

    Add New Features 2025-02-12-00.23.02

---
+++ Sinhala_Bible_Reading_Create_Assist_System/Word_Templates/Start_Docx_For_Mass_Reading.bat
@@ -0,0 +1,49 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJHqM4VAzIFZkXgGROWWuB6EV5+a22+WIrl4hZ+sxcI7X24usKeIf73rWcJcp33Rdp+geCRpMey2KfRw9unp9hX2WI8KRjzjuWEy20k49C2l1k2bDsBgvYcF8pecM0DiB2kLtoIgRwmvAbKwPEWrvxuJtIMxg
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+start "" "C:\Church-Presentation-Tools\Sinhala_Bible_Reading_Create_Assist_System\Word_Templates\1.1.First_Reading.docx"
+timeout /t 3 /nobreak >nul
+start "" "C:\Church-Presentation-Tools\Sinhala_Bible_Reading_Create_Assist_System\Word_Templates\1.2.Responsorial_Psalm.docx"
+timeout /t 0.5 /nobreak >nul
+start "" "C:\Church-Presentation-Tools\Sinhala_Bible_Reading_Create_Assist_System\Word_Templates\1.3.Second_Reading.docx"
+timeout /t 0.5 /nobreak >nul
+start "" "C:\Church-Presentation-Tools\Sinhala_Bible_Reading_Create_Assist_System\Word_Templates\1.4.Aleluya.docx"
+timeout /t 0.5 /nobreak >nul
+start "" "C:\Church-Presentation-Tools\Sinhala_Bible_Reading_Create_Assist_System\Word_Templates\1.5.Gospel.docx"
+timeout /t 0.5 /nobreak >nul
+start "" "C:\Church-Presentation-Tools\Sinhala_Bible_Reading_Create_Assist_System\Word_Templates\2.1.Introduction.docx"
+timeout /t 0.5 /nobreak >nul
+start "" "C:\Church-Presentation-Tools\Sinhala_Bible_Reading_Create_Assist_System\Word_Templates\2.2.The_Prayer_of_the_Faithful_(Bidding_Prayers).docx"
+
+
