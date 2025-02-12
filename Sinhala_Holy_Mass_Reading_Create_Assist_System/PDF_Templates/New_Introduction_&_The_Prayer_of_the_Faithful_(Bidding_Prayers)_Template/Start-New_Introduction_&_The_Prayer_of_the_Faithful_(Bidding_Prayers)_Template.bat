c140ffe32d10ebd5d93a01814c624106329e9725
plr7edu <plr7.edu@outlook.com>
2025-02-12 08:02:13 +0530

    Fix New Feature 2025-02-12-08.02.12

---
+++ Sinhala_Holy_Mass_Reading_Create_Assist_System/PDF_Templates/New_Introduction_&_The_Prayer_of_the_Faithful_(Bidding_Prayers)_Template/Start-New_Introduction_&_The_Prayer_of_the_Faithful_(Bidding_Prayers)_Template.bat
@@ -0,0 +1,40 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJHqM4VAzIFZkXgGROWWuB6EV5+a22+WIrl4hZ+sxcI7X24umL+wK1WjlZoUS5H9bnMICCyR7bBeqegoLiH1Rv3eRCPSFoBvkR3S5wm0PL2B5l2/RmC4oXP1tjfwq3TSs80nqnLEZ3nbAGJY6HWbe8b5pIcwSwgWyV13An4xzS//6Y7v8DwmAAWAbiHnNjLxajII0WjJmBydujrR87SrsRq6IyHZTPX7rofvb4VcgddX/eacNgBK5+Xdh+qG2qG0bA3pLC28USSifSCjiMQ7I0bTWeA9c6PS3MaprbG4EdPgXATgbwqU73TG5k/YtKs3ygy6Pag==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+REM This batch script opens the CorelDRAW file at the specified location
+
+SET "FILE_PATH=C:\Church-Presentation-Tools\Sinhala_Holy_Mass_Reading_Create_Assist_System\PDF_Templates\New_Introduction_&_The_Prayer_of_the_Faithful_(Bidding_Prayers)_Template\New_Introduction_&_The_Prayer_of_the_Faithful_(Bidding_Prayers)_Template.cdr"
+
+REM Open the CorelDRAW file
+START "" "%FILE_PATH%"
---
+++ Sinhala_Holy_Mass_Reading_Create_Assist_System/Word_Templates/CP-Merged-Docx-To-Desktop.bat
@@ -0,0 +1,41 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJHqM4VAzIFZkXgGROWWuB6EV5+a22+WIrl4hZ+sxcI7X24umL+wK1WjlZoUS5H9bnMICCyR7bBeqegoLiH1Rv3eRCPSFoBvkR3S+6Vk0JFFxinPcjT8+c+9Lqo4u1jK5+Umyu6oTyTXLUeQqEHDq1aN4dswY+Go=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+REM This batch script copies the DOCX file to the desktop with a new name
+
+SET "SOURCE_PATH=C:\Church-Presentation-Tools\Sinhala_Holy_Mass_Reading_Create_Assist_System\Word_Templates\merged_document_with_page_breaks.docx"
+SET "TARGET_PATH=%USERPROFILE%\Desktop\Merged-Holy-Mass-Readings-With-Page-Breaks.docx"
+
+REM Copy the file to the desktop with the new name
+COPY "%SOURCE_PATH%" "%TARGET_PATH%"
